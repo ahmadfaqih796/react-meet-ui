1e13dcc2f90cba99d7c6796d2842142e0a00a201
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2024-12-24 11:31:08 +0700

    button

--- src/components/inputs/button/Button.jsx
+++ src/components/inputs/button/Button.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
-const Button = () => {
-  return <button onClick={() => console.log("hallo")}>Hallo</button>;
+const Button = ({ title, ...props }) => {
+  return (
+    <button
+      {...props}
+      style={{
+        backgroundColor: "red",
+        padding: "10px",
+        color: "white",
+        borderRadius: "5px",
+        cursor: "pointer",
+      }}
+    >
+      {title || "Button"}
+    </button>
+  );
 };
 
 export default Button;
